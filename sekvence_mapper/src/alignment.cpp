3f4c594c48850d5478f31ee5ba2fedd16e46d926
franmicka <fran.micka@fer.hr>
2021-12-11 16:21:20 +0100

    dodan izracun poravnanja za Needleman-Wunsch algoritam

--- sekvence_mapper/src/alignment.cpp
+++ sekvence_mapper/src/alignment.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "alignment.h"
+#include <algorithm>
 
 int Align(const char* query, unsigned int query_len,
     const char* target, unsigned int target_len,
@@ -11,7 +12,7 @@ int Align(const char* query, unsigned int query_len,
     unsigned int* target_begin = nullptr) {   
 
     int V[query_len + 1][target_len + 1];
-
+    int allign = 0;
     //Smith-Waterman
     if (type == LOCAL) {
         int M = 0;
@@ -30,14 +31,39 @@ int Align(const char* query, unsigned int query_len,
         }
 
     }
-
+    //Needleman-Wunsch
     else if (type == GLOBAL) {
+        V[0][0] = 0;
+        int poravnanje = 0;
+        for (int i = 1; i <= query_len; i++) {
+            V[i][0] = V[i-1][0] + gap;
+        }
+        for (int j = 1; j <= target_len; j++) {
+            V[0][j] = V[0][j-1] + gap;
+        }
+        for (int i = 1; i <= query_len; i++) {
+            for (int j = 1; j <= target_len; j++) {
+                
+                if (query[i] == target[j]) {
+                    poravnanje = match;
+                }
+                else{
+                    poravnanje = mismatch;
+                }
 
+                int var1 = V[i-1][j-1] + poravnanje; //M
+                int var2 = V[i-1][j] + gap; //I
+                int var3 = V[i][j-1] + gap; //D
+                
+                V[i][j] = std::max({var1, var2, var3});
+            }
+        }
+        allign = V[query_len][target_len];
     }
 
     else if (type == SEMI_GLOBAL) {
 
     }
 
-
+    return allign;
 }
