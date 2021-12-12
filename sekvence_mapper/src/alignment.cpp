c39485da2b6c9e8fad6fcd28df5450591b048c5e
Matej <matej.maric99@gmail.com>
2021-12-12 20:15:01 +0100

    Promjenjena struktura tablice dinamickog programiranja

--- sekvence_mapper/src/alignment.cpp
+++ sekvence_mapper/src/alignment.cpp
@@ -11,18 +11,19 @@ int Align(const char* query, unsigned int query_len,
     std::string* cigar = nullptr,
     unsigned int* target_begin = nullptr) {   
 
-    int V[query_len + 1][target_len + 1];
+    //Dynamic programming table
+    Cell V[query_len + 1][target_len + 1];
     int allign = 0;
     //Smith-Waterman
     if (type == LOCAL) {
         int M = 0;
-        V[0][0] = 0;
+        V[0][0].cost = 0;
 
         for (int i = 0; i <= query_len; i++) {
-            V[i][0] = 0;
+            V[i][0].cost = 0;
         }
         for (int j = 0; j <= target_len; j++) {
-            V[0][j] = 0;
+            V[0][j].cost = 0;
         }
         for (int i = 1; i <= query_len; i++) {
             for (int j = 1; j <= target_len; j++) {
@@ -33,13 +34,13 @@ int Align(const char* query, unsigned int query_len,
     }
     //Needleman-Wunsch
     else if (type == GLOBAL) {
-        V[0][0] = 0;
+        V[0][0].cost = 0;
         int poravnanje = 0;
         for (int i = 1; i <= query_len; i++) {
-            V[i][0] = V[i-1][0] + gap;
+            V[i][0].cost = V[i-1][0].cost + gap;
         }
         for (int j = 1; j <= target_len; j++) {
-            V[0][j] = V[0][j-1] + gap;
+            V[0][j].cost = V[0][j-1].cost + gap;
         }
         for (int i = 1; i <= query_len; i++) {
             for (int j = 1; j <= target_len; j++) {
@@ -51,14 +52,14 @@ int Align(const char* query, unsigned int query_len,
                     poravnanje = mismatch;
                 }
 
-                int var1 = V[i-1][j-1] + poravnanje; //M
-                int var2 = V[i-1][j] + gap; //I
-                int var3 = V[i][j-1] + gap; //D
+                int var1 = V[i-1][j-1].cost + poravnanje; //M
+                int var2 = V[i-1][j].cost + gap; //I
+                int var3 = V[i][j-1].cost + gap; //D
                 
-                V[i][j] = std::max({var1, var2, var3});
+                V[i][j].cost = std::max({var1, var2, var3});
             }
         }
-        allign = V[query_len][target_len];
+        allign = V[query_len][target_len].cost;
     }
 
     else if (type == SEMI_GLOBAL) {
--- sekvence_mapper/src/alignment.h
+++ sekvence_mapper/src/alignment.h
@@ -1,6 +1,13 @@
 #include <iostream>
 
 enum AlignmentType {GLOBAL, LOCAL, SEMI_GLOBAL};
+enum AlignmentTrack {STOP, LEFT, UP, DIAGONAL};
+
+struct Cell {
+    int cost;
+    int parent;
+};
+
 
 int Align(const char* query, unsigned int query_len,
           const char* target, unsigned int target_len,
