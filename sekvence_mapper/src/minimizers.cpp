2d9a64a154ffdd1bb4ae44e7400b5720e7b98287
Matej <matej.maric99@gmail.com>
2021-12-13 20:52:49 +0100

    postavljen minimizer

---
+++ sekvence_mapper/src/minimizers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+std::vector<std::tuple<unsigned int, unsigned int, bool>>
+Minimize(const char *sequence, unsigned int sequence_len, unsigned int kmer_len, unsigned int window_len) {
+
+
+   return;
+}
