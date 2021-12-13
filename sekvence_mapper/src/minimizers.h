ee00b059d1580d9b8610a0cab108a0dc9a175812
Matej <matej.maric99@gmail.com>
2021-12-13 20:31:15 +0100

    minor fixes

---
+++ sekvence_mapper/src/minimizers.h
@@ -0,0 +1,6 @@
+#include <vector>
+
+std::vector<std::tuple<unsigned int, unsigned int, bool>> Minimize(
+    const char* sequence, unsigned int sequence_len,
+    unsigned int kmer_len,
+    unsigned int window_len);
